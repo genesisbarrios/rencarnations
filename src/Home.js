749c30225aceb1bdb8127636e52448a28b3a131c
Genesis Barrios <genesisbarriosdev@gmail.com>
2024-01-22 17:03:20 -0500

    new video - sleep apnea

--- src/Home.js
+++ src/Home.js
@@ -51,20 +51,26 @@ const Home = (props) => {
             
               <p style={{textAlign:"center", fontFamily:"Arial", fontSize:"13px"}}><a href="/StayInTouch" className="links" style={{textDecoration:"none", color:"#e4ddf3"}}>stay in touch</a>  </p>
        
-            <div style={{marginTop:"10%", textAlign:"center", fontFamily:"Arial", marginBottom:"10px", color:'white'}}>
-             <p className="mt-5 mb-2" style={{color:"white", fontSize:'10px', letterSpacing:"2px", paddingBottom:"5%"}}>July 5th, 2020</p>
-             <iframe width="80%" height="600px" src="https://www.youtube.com/embed/hQq3hcBkdVg?si=Ys8Zc6TQ4oMU-KA9" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-             <br></br>
-           
+            <div style={{textAlign:"center", fontFamily:"Arial", marginBottom:"8%", color:'white'}}>
+              <p className="mt-5 mb-2" style={{color:"white", fontSize:'10px', letterSpacing:"2px", paddingBottom:"5%"}}>Nov 17th, 2023</p>
+              <iframe width="80%" height="600px" src="https://www.youtube.com/embed/Fh3t2rFjB08?si=dAlOgWw6samFXFoO" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+              <p className="m-auto mt-2 mb-2 w-50" style={{fontSize:'13px', paddingTop:"5%"}}>{"sleep apnea. out now"}</p>
+              {/* <Button className="mb-5" style={{border:"1px solid #cec09e", color:"#cec09e"}}><a href="https://www.youtube.com/embed/EXAYYSt9Yqw?si=785R3AD6yA4AQY8q" target="_blank" style={{textDecoration:"none", color:"#cec09e"}}>Share</a></Button> */}
             </div>
-            
+       
             <div style={{textAlign:"center", fontFamily:"Arial", marginBottom:"8%", color:'white'}}>
               <p className="mt-5 mb-2" style={{color:"white", fontSize:'10px', letterSpacing:"2px", paddingBottom:"5%"}}>July 28th, 2023</p>
               <iframe width="80%" height="600px" src="https://www.youtube.com/embed/EXAYYSt9Yqw?si=785R3AD6yA4AQY8q" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
               <p className="m-auto mt-2 mb-2 w-50" style={{fontSize:'13px', paddingTop:"5%"}}>{"for someone special <3"}</p>
               {/* <Button className="mb-5" style={{border:"1px solid #cec09e", color:"#cec09e"}}><a href="https://www.youtube.com/embed/EXAYYSt9Yqw?si=785R3AD6yA4AQY8q" target="_blank" style={{textDecoration:"none", color:"#cec09e"}}>Share</a></Button> */}
             </div>
-            
+
+            <div style={{marginTop:"10%", textAlign:"center", fontFamily:"Arial", marginBottom:"10px", color:'white'}}>
+             <p className="mt-5 mb-2" style={{color:"white", fontSize:'10px', letterSpacing:"2px", paddingBottom:"5%"}}>July 5th, 2020</p>
+             <iframe width="80%" height="600px" src="https://www.youtube.com/embed/hQq3hcBkdVg?si=Ys8Zc6TQ4oMU-KA9" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+             <br></br>
+           
+            </div>
        
             <div className="m-auto mt-2 mb-2 w-50" style={{textAlign:"center"}}>
               <p>
